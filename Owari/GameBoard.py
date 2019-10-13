31b9a00d2a54c429a53fde278a56f95e0f994a5f
Your Name <you@example.com>
2019-10-13 15:40:54 -0800

    Commented in pseudocode for GameBoard and MiniMax classes.

--- Owari/GameBoard.py
+++ Owari/GameBoard.py
@@ -1,3 +1,31 @@
 # Zoe Harris & Rachel Lewis
 # Programming Assignment #3
 # CSCE405 Artificial Intelligence
+
+
+"""
+
+class GameBoard:
+
+    def __init___():
+
+        self.owari_board = ???
+
+    def sow(pit):
+        sow seeds across board starting with given pit
+
+    def game_over():
+        check if all six of either players' pits are empty
+        if so, return True
+
+    def display
+        print formatted self.owari_board
+
+    def my_seeds()
+        return # of seeds in my goal
+
+    def opponent_seeds()
+        return # of seeds in opponent's goal
+
+
+"""
